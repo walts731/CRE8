f501d75849e4313b78888dd5f94df5ef5b45ab59
walts731 <142887725+walts731@users.noreply.github.com>
2023-10-27 17:15:09 +1000

    delete bootstrap file to img

---
+++ admin_area/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+    <!-- bootstrap css link -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+    <!-- navbar -->
+    <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg navbar-light bg-info">
+            <div class="container-fluid">
+                <img src="../img/C100.png" alt="">
+            </div>
+        </nav>
+    </div>
+
+    <!-- bootstrap js link -->
+    <script src="js/bootstrap.js"></script>
+</body>
+</html>

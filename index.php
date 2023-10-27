6931cee9bf9cf6b7951f8729772d6d96f1f4eb2e
walts731 <142887725+walts731@users.noreply.github.com>
2023-10-27 10:43:44 +1000

    first commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRE8</title>
+    <!-- bootstrap css link -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <!-- font awesome link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <!-- userdefinedstyle css --> 
+    <link rel="stylesheet" href="userdefinedstyle.css">
+</head>
+<body>
+    <!-- nav bar -->
+    <div class="container-fluid p-0">
+        <!-- first child -->
+        <nav class="navbar navbar-expand-lg bg-info">
+  <div class="container-fluid">
+    <img src="./img/C100.png" alt="" class="logo">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Products</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Register</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="fa-solid fa-cart-shopping"><sup>1</sup></i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Total price:100/-</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-light" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+  <!-- last child -->
+  <div class="bg-info p-3 text-center">
+    <p>All rights reserved | CRUD 2023</p>
+  </div>
+    </div>
+</body>
+    <!-- javascript link -->
+    <script src="js/bootstrap.js"></script>
+</html>

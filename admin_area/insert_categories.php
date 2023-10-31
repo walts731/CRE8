990f14d934644e1f63e8376740d4d4d1e38f88ed
walts731 <142887725+walts731@users.noreply.github.com>
2023-10-31 14:40:49 +1000

    insert and select query into categories

--- admin_area/insert_categories.php
+++ admin_area/insert_categories.php
@@ -1,7 +1,22 @@
 <?php
 include('../includes/connect.php');
+if(isset($_POST['insert_cat'])){
+  $category_title=$_POST['cat_title'];
 
-
+  // select data from database
+  $select_query="Select * from `categories` where category_title='$category_title'";
+  $result_select=mysqli_query($con,$select_query);
+  $number=mysqli_num_rows($result_select);
+  if($number>0){
+    echo "<script>alert ('This category is present inside the database')</script>";
+  }else{
+  $insert_query="insert into `categories` (category_title) value ('$category_title')";
+  $result=mysqli_query($con,$insert_query);
+  if($result){
+    echo "<script>alert ('Category has been inserted successfully!')</script>";
+  }
+}
+}
 ?>
 
 
@@ -12,7 +27,7 @@ include('../includes/connect.php');
 </div>
 <div class="input-group w-10 mb-2 m-auto">
   
-  <!--<input type="submit" class="form-control" name="insert_cat" value="Insert Categories" aria-label="Username" aria-describedby="basic-addon1" class="bg-info">-->
-  <button class="bg-info p-2 my-3 border-0">Insert Categories</button>
+  <input type="submit" class="bg-info border-0 p-2 my-3" name="insert_cat" value="Insert Categories">
+  
 </div>
 </form>

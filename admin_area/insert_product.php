fee4713aa20313ea4f5a7630d3340165200e0d59
walts731 <142887725+walts731@users.noreply.github.com>
2023-11-02 19:42:13 +1000

    displaying dynamic products

--- admin_area/insert_product.php
+++ admin_area/insert_product.php
@@ -1,5 +1,41 @@
 <?php
 include('../includes/connect.php');
+if(isset($_POST['insert_product'])){
+    $product_title=$_POST['product_title'];
+    $description=$_POST['description'];
+    $product_keywords=$_POST['product_keywords'];
+    $product_categories=$_POST['product_categories'];
+    $product_brands=$_POST['product_brands'];
+    $product_price=$_POST['product_price'];
+    $product_status='true';
+
+    //accessing images
+    $product_image1=$_FILES['product_image1']['name'];
+    $product_image2=$_FILES['product_image2']['name'];
+    $product_image3=$_FILES['product_image3']['name'];
+
+    //accessing image tmp name
+    $temp_image1=$_FILES['product_image1']['tmp_name'];
+    $temp_image2=$_FILES['product_image2']['tmp_name'];
+    $temp_image3=$_FILES['product_image3']['tmp_name'];
+
+    //checking empty condition
+    if($product_title=='' or $description=='' or $product_keywords=='' or $product_categories=='' or $product_brands=='' or $product_price=='' or $product_image1=='' or $product_image2=='' or $product_image3==''){
+        echo "<script>alert('Please fill all the available fields')</script>";
+        exit();
+    }else{
+        move_uploaded_file($temp_image1,"./product_images/$product_image1");
+        move_uploaded_file($temp_image2,"./product_images/$product_image2");
+        move_uploaded_file($temp_image3,"./product_images/$product_image3");
+
+        //insert query
+        $insert_products="insert into `products` (product_title, product_description, product_keywords, category_id, brand_id, product_image1, product_image2, product_image3, product_price, date, status) values('$product_title','$description', '$product_keywords', '$product_categories', '$product_brands', '$product_image1','$product_image2', '$product_image3', '$product_price', NOW(), '$product_status')";
+        $result_query=mysqli_query($con,$insert_products);
+        if($result_query){
+            echo "<script>alert('Successfully inserted the products')</script>";
+        }
+    }
+}
 ?>
 
 
@@ -42,10 +78,10 @@ include('../includes/connect.php');
                     <option value="">Select Category</option>
                     <?php
                     $select_query="Select * from `categories`";
-                    $result_query=mysqli_query($con,$select_query)
+                    $result_query=mysqli_query($con,$select_query);
                     while($row=mysqli_fetch_assoc($result_query)){
-                        $category_title=$row['category_title']
-                        $category_id=$row['category_id']
+                        $category_title=$row['category_title'];
+                        $category_id=$row['category_id'];
                         echo "<option value='$category_id'> $category_title</option>";
                     }
 
@@ -58,10 +94,10 @@ include('../includes/connect.php');
                     <option value="">Select Brands</option>
                     <?php
                     $select_query="Select * from `brands`";
-                    $result_query=mysqli_query($con,$select_query)
+                    $result_query=mysqli_query($con,$select_query);
                     while($row=mysqli_fetch_assoc($result_query)){
-                        $brand_title=$row['brand_title']
-                        $brand_id=$row['brand_id']
+                        $brand_title=$row['brand_title'];
+                        $brand_id=$row['brand_id'];
                         echo "<option value='$brand_id'>$brand_title</option>";
                     }
 
--- index.php
+++ index.php
@@ -76,76 +76,40 @@ include('includes/connect.php');
   <!-- fourth child -->
   <div class="row">
     <div class="col-md-10">
+      <!-- products -->
       <div class="row">
-        <div class="col-md-4 mb-2">
-        <div class="card">
-  <img src="./img/dress.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-info">Add to Cart</a>
-    <a href="#" class="btn btn-secondary">View more</a>
-  </div>
-</div>
-        </div>
-        <div class="col-md-4 mb-2">
-        <div class="card">
-  <img src="./img/shirt.webp" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-info">Add to Cart</a>
-    <a href="#" class="btn btn-secondary">View more</a>
-  </div>
-</div>
-        </div>
-        <div class="col-md-4 mb-2">
-        <div class="card">
-  <img src="./img/pants.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-info">Add to Cart</a>
-    <a href="#" class="btn btn-secondary">View more</a>
-  </div>
-</div>
-        </div>
-        <div class="col-md-4 mb-2">
-        <div class="card">
-  <img src="./img/pants.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-info">Add to Cart</a>
-    <a href="#" class="btn btn-secondary">View more</a>
-  </div>
-</div>
-        </div>
-        <div class="col-md-4 mb-2">
-        <div class="card">
-  <img src="./img/pants.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-info">Add to Cart</a>
-    <a href="#" class="btn btn-secondary">View more</a>
-  </div>
-</div>
-        </div>
-        <div class="col-md-4 mb-2">
-        <div class="card">
-  <img src="./img/pants.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-info">Add to Cart</a>
-    <a href="#" class="btn btn-secondary">View more</a>
-  </div>
-</div>
-        </div>
+  <!-- fetching products -->
+      <?php
+  $select_query="Select * from `products` order by rand() LIMIT 0,9";
+  $result_query=mysqli_query($con, $select_query);
+  //$row=mysqli_fetch_assoc($result_query);
+  //echo $row['product_title'];
+  while( $row=mysqli_fetch_assoc($result_query)){
+    $product_id=$row['product_id'];
+    $product_title=$row['product_title'];
+    $product_description=$row['product_description'];
+    $product_image1=$row['product_image1'];
+    $product_price=$row['product_price'];
+    $category_id=$row['category_id'];
+    $brand_id=$row['brand_id'];
+    echo "<div class='col-md-4 mb-2'>
+    <div class='card'>
+              <img src='./admin_area/product_images/$product_image1' class='card-img-top' alt='$product_title'>
+              <div class='card-body'>
+                <h5 class='card-title'>$product_title</h5>
+                <p class='card-text'>$product_description</p>
+                <a href='#' class='btn btn-info'>Add to Cart</a>
+                <a href='#' class='btn btn-secondary'>View more</a>
+              </div>
       </div>
-    </div>
-    <!-- products -->
+    </div>";
+  }
+      ?>
+
+<!-- row end -->
+</div>
+<!-- col end -->
+</div>
     <div class="col-md-2 bg-secondary p-0">
       <!-- brands -->
       <ul class="navbar-nav me-auto text-center">

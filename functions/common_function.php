c591b0d96ff63476a92c1395e2aad67ff5d565f3
walts731 <142887725+walts731@users.noreply.github.com>
2023-11-04 18:59:05 +1000

    view more logic

--- functions/common_function.php
+++ functions/common_function.php
@@ -222,6 +222,62 @@ function search_product(){
     }
 }
 }
+
+
+// view details function
+function view_details(){
+  global $con;
+
+    //condition to check isset or not
+    if(isset($_GET['product_id'])){
+    if(!isset($_GET['category'])){
+      if(!isset($_GET['brand'])){
+        $product_id=$_GET['product_id'];
+    $select_query="Select * from `products` where product_id=$product_id";
+    $result_query=mysqli_query($con, $select_query);
+    //$row=mysqli_fetch_assoc($result_query);
+    //echo $row['product_title'];
+    while( $row=mysqli_fetch_assoc($result_query)){
+      $product_id=$row['product_id'];
+      $product_title=$row['product_title'];
+      $product_description=$row['product_description'];
+      $product_image1=$row['product_image1'];
+      $product_image2=$row['product_image2'];
+      $product_image3=$row['product_image3'];
+      $product_price=$row['product_price'];
+      $category_id=$row['category_id'];
+      $brand_id=$row['brand_id'];
+      echo "<div class='col-md-4 mb-2'>
+      <div class='card'>
+                <img src='./admin_area/product_images/$product_image1' class='card-img-top' alt='$product_title'>
+                <div class='card-body'>
+                  <h5 class='card-title'>$product_title</h5>
+                  <p class='card-text'>$product_description</p>
+                  <a href='#' class='btn btn-info'>Add to Cart</a>
+                  <a href='product_details.php?product_id=$product_id' class='btn btn-secondary'>View more</a>
+                </div>
+        </div>
+      </div>
+      
+      <div class='col-md-8'>
+        <!-- related images -->
+        <div class='row'>
+            <div class='col-md-12'>
+                <h4 class='text-center text-info mb-5'>Related products</h4>
+            </div>
+            <div class='col-md-6'>
+                <img src='./admin_area/product_images/$product_image2' class='card-img-top' alt='$product_title'>
+            </div>
+            <div class='col-md-6'>
+                <img src='./admin_area/product_images/$product_image3' class='card-img-top' alt='$product_title'>
+            </div>
+        </div>
+      </div>";
+    }
+}
+}
+}
+}
 ?>
 
 
--- product_details.php
+++ product_details.php
@@ -81,36 +81,11 @@ include('functions/common_function.php');
       <!-- products -->
       <div class="row">
 
-      <div class="col-md-4">
-        <!-- card -->
-        <div class='card'>
-                <img src='./img/dress.jpg' class='card-img-top' alt='$product_title'>
-                <div class='card-body'>
-                  <h5 class='card-title'>$product_title</h5>
-                  <p class='card-text'>$product_description</p>
-                  <a href='#' class='btn btn-info'>Add to Cart</a>
-                  <a href='product_details.php?product_id=$product_id' class='btn btn-secondary'>View more</a>
-                </div>
-        </div>
-      </div>
-
-      <div class="col-md-8">
-        <!-- related images -->
-        <div class="row">
-            <div class="col-md-12">
-                <h4 class="text-center text-info mb-5">Related products</h4>
-            </div>
-            <div class="col-md-6">
-                <img src='./img/blue_dress.jpg' class='card-img-top' alt='$product_title'>
-            </div>
-            <div class="col-md-6">
-                <img src='./img/white_dress.jpg' class='card-img-top' alt='$product_title'>
-            </div>
-        </div>
-      </div>
+      
   <!-- fetching products -->
       <?php
       // calling function
+      view_details();
   get_unique_categories();
   get_unique_brands();
       ?>

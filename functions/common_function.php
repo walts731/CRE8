1fa1ce60c90f405bedef2260b6c139ee128655bf
walts731 <142887725+walts731@users.noreply.github.com>
2023-11-05 14:08:47 +1000

    ip address

--- functions/common_function.php
+++ functions/common_function.php
@@ -283,15 +283,15 @@ function view_details(){
 function getIPAddress(){
   // whether ip is from the share internet
   if(!empty($_SERVER['HTTP_CLIENT_IP'])){
-    $ip=$SERVER['HTTP_CLIENT_IP'];
+    $ip=$_SERVER['HTTP_CLIENT_IP'];
   }
   //whether ip is from proxy
-  elseif(!empty($SERVER['HTTP_X_FORWARDED_FOR'])){
-    $ip=$SERVER['HTTP_X_FORWARDED_FOR'];
+  elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+    $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
   }
   //whether ip is from the remote address
   else{
-    $ip=$SERVER['REMOTE_ADDR'];
+    $ip=$_SERVER['REMOTE_ADDR'];
   }
   return $ip;
 }
@@ -317,6 +317,26 @@ if(isset($_GET['add_to_cart'])){
     echo "<script>window.open('index.php','_self')</script>";
   }
 }
+}
+
+// function to get cart item numbers
+function cart_item(){
+  if(isset($_GET['add_to_cart'])){
+    global $con;
+    $get_ip_add=getIPAddress(); 
+    $select_query="Select * from `cart_details` where ip_address='$get_ip_add'";
+    $result_query=mysqli_query($con, $select_query);
+    $num_of_rows=mysqli_num_rows($result_query);
+    if($num_of_rows>0){
+      echo "<script>alert('This item is already present inside the cart')</script>";
+      echo "<script>window.open('index.php','_self')</script>";
+    }else{
+      $insert_query="insert into `cart_details` (product_id,ip_address,quantity) values ($get_product_id,'$get_ip_add',0)";
+      $result_query=mysqli_query($con, $insert_query);
+      echo "<script>alert('Item is added to cart')</script>";
+      echo "<script>window.open('index.php','_self')</script>";
+    }
+  }
 }
 ?>
 

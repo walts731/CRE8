9e406803f7ac705b39fd67c2818772c17fdedae0
walts731 <142887725+walts731@users.noreply.github.com>
2023-11-05 18:11:53 +1000

    fetching cart items

--- cart.php
+++ cart.php
@@ -16,6 +16,13 @@ include('functions/common_function.php');
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <!-- userdefinedstyle css --> 
     <link rel="stylesheet" href="userdefinedstyle.css">
+    <style>
+        .cart_img{
+    width:50px;
+    height:50px;
+    object-fit:contain;
+}
+    </style>
 </head>
 <body>
     <!-- nav bar -->
@@ -82,26 +89,50 @@ include('functions/common_function.php');
                         <th>Quantity</th>
                         <th>Total Price</th>
                         <th>Remove</th>
-                        <th>Operations</th>
+                        <th colspan="2">Operations</th>
                     </tr>
                 </thead>
                 <tbody>
+                    <!-- php code to display dynamic data -->
+                    <?php
+                        global $con;
+                        $get_ip_add=getIPAddress(); 
+                        $total_price=0;
+                        $cart_query="Select * from `cart_details` where ip_address='$get_ip_add'";
+                        $result=mysqli_query($con, $cart_query);
+                        while($row=mysqli_fetch_array($result)){
+                          $product_id=$row['product_id'];
+                          $select_products="Select * from `products` where product_id='$product_id'";
+                          $result_products=mysqli_query($con, $select_products);
+                          while($row_product_price=mysqli_fetch_array($result_products)){
+                            $product_price=array($row_product_price['product_price']); 
+                            $price_table=$row_product_price['product_price'];
+                            $product_title=$row_product_price['product_title'];
+                            $product_image1=$row_product_price['product_image1'];
+                            $product_values=array_sum($product_price); //[500]
+                            $total_price+=$product_values; //[500]
+                        
+                    ?>
                     <tr>
-                        <td>Dress</td>
-                        <td><img src="./img/dress.jpg" alt=""></td>
-                        <td><input type="text"></td>
-                        <td>300</td>
+                        <td><?php echo $product_title?></td>
+                        <td><img src="./img/<?php echo $product_image1?>" alt="" class="cart_img"></td>
+                        <td><input type="text" name="" id="" class="form-input w-50"></td>
+                        <td><?php echo $price_table?></td>
                         <td><input type="checkbox"></td>
                         <td>
-                            <p>Update</p>
-                            <p>Remove</p>
+                            <button class="bg-info px-3 py-2 border-0 mx-3">Update</button>
+                            <button class="bg-info px-3 py-2 border-0 mx-3">Remove</button>
                         </td>
                     </tr>
+                    <?php
+                    }
+                }
+                    ?>
                 </tbody>
             </table>
             <!-- subtotal -->
             <div class="d-flex mb-5">
-                <h4 class="px-3">Subtotal:<strong class="text-info">300</strong></h4>
+                <h4 class="px-3">Subtotal:<strong class="text-info"><?php echo $total_price?></strong></h4>
                 <a href="index.php"><button class="bg-info px-3 py-2 border-0 mx-3">Continue Shopping</button></a>
                 <a href="#"><button class="bg-secondary px-3 py-2 border-0 text-light">Checkout</button></a>
             </div>

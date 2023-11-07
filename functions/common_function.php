1680996e0f2f4d301c223b61d395c1cb5b272eb1
walts731 <142887725+walts731@users.noreply.github.com>
2023-11-07 20:19:22 +1000

    inserting user data

--- functions/common_function.php
+++ functions/common_function.php
@@ -1,7 +1,7 @@
 <?php
 
 // including connect file
-include('./includes/connect.php');
+///include('./includes/connect.php');
 
 
 //getting products
--- users_area/user_registration.php
+++ users_area/user_registration.php
@@ -1,3 +1,9 @@
+<?php
+include('../includes/connect.php');
+include('../functions/common_function.php');
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -62,3 +68,29 @@
 
 </body>
 </html>
+
+<!-- php code -->
+<?php
+if(isset($_POST['user_register'])){
+    $user_username=$_POST['user_username'];
+    $user_email=$_POST['user_email'];
+    $user_password=$_POST['user_password'];
+    $conf_user_password=$_POST['conf_user_password'];
+    $user_address=$_POST['user_address'];
+    $user_contact=$_POST['user_contact'];
+    $user_image=$_FILES['user_image']['name'];
+    $user_image_tmp=$_FILES['user_image']['tmp_name'];
+    $user_ip=getIPAddress();
+
+    // insert query
+    move_uploaded_file($user_image_tmp, "./user_images/$user_image");
+    $insert_query="insert into `user_table` (username, user_email, user_password, user_image, user_ip, user_address, user_mobile) values('$user_username','$user_email',' $user_password','$user_image',' $user_ip','$user_address',' $user_contact')";
+    $sql_execute=mysqli_query($con, $insert_query);
+    if($sql_execute){
+        echo "<script>alert('Data inserted successfully!')</script>";
+    }else{
+        die(mysqli_error($con));
+    }
+}
+
+?>

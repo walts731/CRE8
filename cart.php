142a18957aeb8214152e5bf7d34419b73833cdaa
walts731 <142887725+walts731@users.noreply.github.com>
2023-11-07 11:38:55 +1000

    working on checkout page and user_area

--- cart.php
+++ cart.php
@@ -162,7 +162,7 @@ include('functions/common_function.php');
                if($result_count>0){
                 echo "<h4 class='px-3'>Subtotal:<strong class='text-info'>$total_price</strong></h4>
                 <input type='submit' value='Continue Shopping' class='bg-info px-3 py-2 border-0 mx-3' name='continue_shopping'>
-                <a href=''><button class='bg-secondary px-3 py-2 border-0 text-light'>Checkout</button></a>";
+                <button class='bg-secondary px-3 py-2 border-0'><a href='checkout.php' class='text-light text-decoration-none'>Checkout</a></button>";
                }else{
                 echo "<input type='submit' value='Continue Shopping' class='bg-info px-3 py-2 border-0 mx-3' name='continue_shopping'>";
                }
---
+++ checkout.php
@@ -0,0 +1,98 @@
+<!-- connect file-->
+<?php
+include('includes/connect.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRE8 | Checkout Page</title>
+    <!-- bootstrap css link -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <!-- font awesome link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <!-- userdefinedstyle css --> 
+    <link rel="stylesheet" href="userdefinedstyle.css">
+</head>
+<body>
+    <!-- nav bar -->
+    <div class="container-fluid p-0">
+        <!-- first child -->
+        <nav class="navbar navbar-expand-lg bg-info">
+  <div class="container-fluid">
+    <img src="./img/C100.png" alt="" class="logo">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="display_all.php">Products</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Register</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
+      <form class="d-flex" action="search_product.php" method="get">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="search_data">
+        <!-- <button class="btn btn-outline-light" type="submit">Search</button> -->
+        <input type="submit" value="search" class="btn btn-outline-light" name="search_data_product">
+      </form>
+    </div>
+  </div>
+</nav>
+
+  <!-- second child -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+    <ul class="navbar-nav me-auto">
+    <li class="nav-item">
+          <a class="nav-link" href="#">Welcome Guest</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Login</a>
+        </li>
+    </ul>
+  </nav>
+
+  <!-- third child -->
+  <div class="bg-light">
+    <h3 class="text-center">CRE8</h3>
+    <p class="text-center">Where Style Meets Confidence!</p>
+  </div>
+
+  <!-- fourth child -->
+  <div class="row px-1">
+    <div class="col-md-12">
+      <!-- products -->
+      <div class="row">
+        <?php
+        if(!isset($_SESSION['username'])){
+            include('users_area/user_login.php');
+        }else{
+            include('payment.php');
+        }
+        ?>
+<!-- row end -->
+</div>
+<!-- col end -->
+</div>
+    
+  </div>
+
+  <!-- last child -->
+  <!-- include footer -->
+  <?php include("./includes/footer.php")?>  
+    </div>
+</body>
+    <!-- javascript link -->
+    <script src="js/bootstrap.js"></script>
+</html>

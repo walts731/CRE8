a3e3dae892b60d27caba52c18244dc69caa411bf
walts731 <142887725+walts731@users.noreply.github.com>
2023-11-07 22:13:24 +1000

    if items are present in cart or not

--- users_area/user_registration.php
+++ users_area/user_registration.php
@@ -98,7 +98,18 @@ if(isset($_POST['user_register'])){
     $insert_query="insert into `user_table` (username, user_email, user_password, user_image, user_ip, user_address, user_mobile) values('$user_username','$user_email',' $hash_password','$user_image',' $user_ip','$user_address',' $user_contact')";
     $sql_execute=mysqli_query($con, $insert_query);
     }
-    
+
+    // selecting cart items
+    $select_cart_items="Select * from `cart_details` where ip_address='$user_ip'";
+    $result_cart=mysqli_query($con, $select_cart_items);
+    $rows_count=mysqli_num_rows($result_cart);
+    if($rows_count>0){
+        $_SESSION['username']=$user_username;
+        echo "<script>alert('You have items in your cart')</script>";
+        echo "<script>window.open('checkout.php','_self')</script>";
+    }else{
+        echo "<script>window.open('../index.php','_self')</script>";
+    }
 }
 
 ?>

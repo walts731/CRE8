072b9c06e0dc1b91d79b1000bd3695038c009f8a
walts731 <142887725+walts731@users.noreply.github.com>
2023-11-07 13:16:15 +1000

    creating registration form

---
+++ users_area/user_registration.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Registration</title>
+     <!-- bootstrap css link -->
+     <link rel="stylesheet" href="../css/bootstrap.css">
+    <!-- userdefinedstyle css --> 
+    <link rel="stylesheet" href="../userdefinedstyle.css">
+</head>
+<body>
+    <div class="container-fluid my-3">
+        <h2 class="text-center">New User Registration</h2>
+        <div class="row d-flex align-items-center justify-content-center">
+            <div class="lg-12 col-xl-6">
+                <form action="" method="post" enctype="multipart/form-data">
+                    <!-- username field -->
+                    <div class="form-outline mb-4">
+                        <label for="user_username" class="form-label">Username</label>
+                        <input type="text" id="user_username" class="form-control" placeholder="Enter your username" autocomplete="off" required="required" name="user_username"/>
+                    </div>
+                    <!-- email field -->
+                    <div class="form-outline mb-4">
+                        <label for="user_email" class="form-label">Email</label>
+                        <input type="email" id="user_email" class="form-control" placeholder="Enter your email" autocomplete="off" required="required" name="user_email"/>
+                    </div>
+                    <!-- image field -->
+                    <div class="form-outline mb-4">
+                        <label for="user_image" class="form-label">User Image</label>
+                        <input type="file" id="user_image" class="form-control" required="required" name="user_image"/>
+                    </div>
+                    <!-- password field -->
+                    <div class="form-outline mb-4">
+                        <label for="user_password" class="form-label">Password</label>
+                        <input type="password" id="user_password" class="form-control" placeholder="Enter your password" autocomplete="off" required="required" name="user_password"/>
+                    </div>
+                    <!-- confirm password field -->
+                    <div class="form-outline mb-4">
+                        <label for="conf_user_password" class="form-label">Confirm Password</label>
+                        <input type="password" id="conf_user_password" class="form-control" placeholder="Confirm your password" autocomplete="off" required="required" name="conf_user_password"/>
+                    </div>
+                     <!-- address field -->
+                     <div class="form-outline mb-4">
+                        <label for="user_address" class="form-label">Address</label>
+                        <input type="text" id="user_address" class="form-control" placeholder="Enter your address" autocomplete="off" required="required" name="user_address"/>
+                    </div>
+                     <!-- contact field -->
+                     <div class="form-outline mb-4">
+                        <label for="user_contact" class="form-label">Contact</label>
+                        <input type="text" id="user_contact" class="form-control" placeholder="Enter your mobile number" autocomplete="off" required="required" name="user_contact"/>
+                    </div>
+                    <div class="mt-4 pt-2">
+                        <input type="submit" value="Register" class="bg-info py-2 px-3 border-0" name="user_register">
+                        <p class="small fw-bold mt-2 pt-1 mb-0">Already have an account ? <a href="user_login.php" class="text-danger"> Login</a></p>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+</html>

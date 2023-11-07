5110f346d2e03bda89627ada9a71184c4b7bbd9e
walts731 <142887725+walts731@users.noreply.github.com>
2023-11-07 21:19:59 +1000

    PASSWORD HASHING using PASSWORD DEFAULT

--- users_area/user_registration.php
+++ users_area/user_registration.php
@@ -75,6 +75,7 @@ if(isset($_POST['user_register'])){
     $user_username=$_POST['user_username'];
     $user_email=$_POST['user_email'];
     $user_password=$_POST['user_password'];
+    $hash_password=password_hash($user_password,PASSWORD_DEFAULT);
     $conf_user_password=$_POST['conf_user_password'];
     $user_address=$_POST['user_address'];
     $user_contact=$_POST['user_contact'];
@@ -94,7 +95,7 @@ if(isset($_POST['user_register'])){
     else{
         // insert query
     move_uploaded_file($user_image_tmp, "./user_images/$user_image");
-    $insert_query="insert into `user_table` (username, user_email, user_password, user_image, user_ip, user_address, user_mobile) values('$user_username','$user_email',' $user_password','$user_image',' $user_ip','$user_address',' $user_contact')";
+    $insert_query="insert into `user_table` (username, user_email, user_password, user_image, user_ip, user_address, user_mobile) values('$user_username','$user_email',' $hash_password','$user_image',' $user_ip','$user_address',' $user_contact')";
     $sql_execute=mysqli_query($con, $insert_query);
     }
     

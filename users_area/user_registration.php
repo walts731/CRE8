025aeee651432a70bc5bce4e8a21d7d1fb08600f
walts731 <142887725+walts731@users.noreply.github.com>
2023-11-07 20:59:53 +1000

    if username email exist or not & pass!=conf_pass

--- users_area/user_registration.php
+++ users_area/user_registration.php
@@ -82,15 +82,22 @@ if(isset($_POST['user_register'])){
     $user_image_tmp=$_FILES['user_image']['tmp_name'];
     $user_ip=getIPAddress();
 
-    // insert query
+    // select query
+    $select_query="Select * from `user_table` where username='$user_username' or user_email='$user_email'";
+    $result=mysqli_query($con, $select_query);
+    $rows_count=mysqli_num_rows($result);
+    if($rows_count>0){
+        echo "<script>alert('Username and email already exist :>')</script>";
+    }elseif($user_password!= $conf_user_password){
+        echo "<script>alert('Passwords do not match! :>')</script>";
+    }
+    else{
+        // insert query
     move_uploaded_file($user_image_tmp, "./user_images/$user_image");
     $insert_query="insert into `user_table` (username, user_email, user_password, user_image, user_ip, user_address, user_mobile) values('$user_username','$user_email',' $user_password','$user_image',' $user_ip','$user_address',' $user_contact')";
     $sql_execute=mysqli_query($con, $insert_query);
-    if($sql_execute){
-        echo "<script>alert('Data inserted successfully!')</script>";
-    }else{
-        die(mysqli_error($con));
     }
+    
 }
 
 ?>

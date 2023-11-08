3324e2229c09faeb053c3292cb89994cd489a9dc
walts731 <142887725+walts731@users.noreply.github.com>
2023-11-08 17:26:35 +1000

    Login and logout session

--- search_product.php
+++ search_product.php
@@ -2,6 +2,7 @@
 <?php
 include('includes/connect.php');
 include('functions/common_function.php');
+session_start();
 ?>
 
 <!DOCTYPE html>
@@ -36,7 +37,7 @@ include('functions/common_function.php');
           <a class="nav-link" href="display_all.php">Products</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Register</a>
+          <a class="nav-link" href="./users_area/user_registration.php">Register</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Contact</a>
@@ -68,9 +69,17 @@ include('functions/common_function.php');
     <li class="nav-item">
           <a class="nav-link" href="#">Welcome Guest</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="./users_area/user_login.php">Login</a>
-        </li>
+        <?php
+          if(!isset($_SESSION['username'])){
+            echo "<li class='nav-item'>
+            <a class='nav-link' href='./users_area/user_login.php'>Login</a>
+          </li>";
+          }else{
+            echo "<li class='nav-item'>
+            <a class='nav-link' href='./users_area/logout.php'>Logout</a>
+          </li>";
+          }
+        ?>
     </ul>
   </nav>
 
--- users_area/user_login.php
+++ users_area/user_login.php
@@ -1,6 +1,7 @@
 <?php
 include('../includes/connect.php');
 include('../functions/common_function.php');
+@session_start();
 ?>
 
 <!DOCTYPE html>

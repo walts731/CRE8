1a00de3363d50e8da8f81b68ff1c9376cbe44460
walts731 <142887725+walts731@users.noreply.github.com>
2023-11-08 18:47:11 +1000

    payment page pero nagerror array offset

--- users_area/checkout.php
+++ users_area/checkout.php
@@ -11,11 +11,11 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRE8 | Checkout Page</title>
     <!-- bootstrap css link -->
-    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="../css/bootstrap.css">
     <!-- font awesome link -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <!-- userdefinedstyle css --> 
-    <link rel="stylesheet" href="userdefinedstyle.css">
+    <link rel="stylesheet" href="../userdefinedstyle.css">
 </head>
 <body>
     <!-- nav bar -->
--- users_area/payment.php
+++ users_area/payment.php
@@ -1,11 +1,44 @@
+<?php
+include('../includes/connect.php');
+include('../functions/common_function.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Payment page</title>
+     <!-- bootstrap css link -->
+     <link rel="stylesheet" href="../css/bootstrap.css">
+    <!-- userdefinedstyle css --> 
+    <link rel="stylesheet" href="../userdefinedstyle.css">
 </head>
+<style>
+    .payment_img{
+        width:50%;
+        margin:auto;
+        display:block;
+    }
+</style>
 <body>
-    <h1>Payment</h1>
+    <!-- php code to access user id -->
+    <?php
+        $user_ip=getIPAddress();
+        $get_user="Select * from `user_table` where user_ip='$user_ip'";
+        $result=mysqli_query($con,$get_user);
+        $run_query=mysqli_fetch_array($result);
+        $user_id=$run_query['user_id'];
+    ?>
+    <div class="container">
+        <h2 class="text-center text-info">Payment options</h2>
+        <div class="row d-flex justify-content-center align-items-center my-5">
+            <div class="col-md-6">
+                <a href="https://www.gcash.com" target="_blank"><img src="../img/gcash.jpg" alt="" class="payment_img"></a>
+            </div>
+            <div class="col-md-6">
+                <a href="order.php?user_id=<?php echo $user_id ?>"><h2 class="text-center">Pay offline</h2></a>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
